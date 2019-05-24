06e66170aa8cbb9000e84a3ea4d7efb84311606b
Mark Leo Cabang Tarectecan <ratrateroo@gmail.com>
2019-05-25 01:51:38 +0800

    added routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,6 +33,7 @@ class BurgerBuilder extends Component {
     }
     
     componentDidMount () {
+        console.log(this.props);
         axios.get('https://react-my-burger-4e1e8.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data});
@@ -97,28 +98,19 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('You continue!');
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Daisy Johnson',
-                address: {
-                    street: 'Zepher',
-                    zipCode: '12345',
-                    country: 'USA'
-                },
-                email: 'quakegirl@email.com'
-            },
-            deliveryMethod: 'fastest'
+       
+
+        //passing values using query params ===============================================================
+        //for Checkout.js
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false  });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false  });
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString});
     }
     
     render() {
